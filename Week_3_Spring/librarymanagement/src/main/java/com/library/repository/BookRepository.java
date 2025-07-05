c42771bfd273f6219a9e28868723f29be596ae4f
Akancha0108 <kumariakancha74@gmail.com>
2025-07-05 21:12:19 +0530

    Completed Week 4

---
+++ Week_3_Spring/librarymanagement/src/main/java/com/library/repository/BookRepository.java
@@ -0,0 +1,11 @@
+package com.library.repository;
+
+public class BookRepository {
+     public String getBook() {
+        return "Spring in Action";
+    }
+
+    public void printBook() {
+        System.out.println("Book: Spring in Action");
+    }
+}
